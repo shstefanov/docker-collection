46d3927fa0401aa5f389b73d6324d2a74506a1bd
Stefan Stefanov <stefan@debian>
2018-03-29 10:20:44 +0300

    Addin index.sql

--- db/schema/index.sql
+++ db/schema/index.sql
@@ -1,7 +1,3 @@
+-- Setup some sql stuff here
 
-CREATE TABLE users (
-	user_id SERIAL UNIQUE,
-	name   varchar(40) NOT NULL CHECK (name <> ''),
-	email   varchar(40) NOT NULL CHECK (email <> ''),
-	password   varchar(40) NOT NULL CHECK (password <> '')
-);
+-- Most of tables will be created from laravel migrations
